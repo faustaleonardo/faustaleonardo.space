f81581db15c4b675ea4d1a047ae49426bb20d49c
faustaleonardo <faustaleonardo11@gmail.com>
2022-03-09 12:14:03 +0700

    added gradient logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import SunIcon from './icons/SunIcon';
 const Header = () => {
   const { theme, setTheme, isMounted } = useTheme();
   const isLight = theme === LIGHT;
+  const themePosition = isLight ? 'justify-end' : 'justify-start';
 
   const handleToggleTheme = () => {
     const nextTheme = theme === LIGHT ? DARK : LIGHT;
@@ -17,13 +18,11 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between py-6 px-8">
-      <h1>faustaleonardo</h1>
+      <h1 className="text-gradient text-4xl">Fausta</h1>
 
       <div className="flex">
         <button
-          className={`flex items-center w-10 border border-zinc-500 rounded-full justify-${
-            isLight ? 'end' : 'start'
-          }`}
+          className={`flex items-center w-10 border border-zinc-500 rounded-full ${themePosition}`}
           onClick={handleToggleTheme}
         >
           <div className="p-1">{isLight ? <SunIcon /> : <MoonIcon />}</div>
