30293931321192385f5007d17ceb0e6299cde073
faustaleonardo <faustaleonardo11@gmail.com>
2022-03-13 12:15:23 +0700

    Revert "fixed meta tags hardcode"
    This reverts commit 338cc4a864888837c7bb8d83c4a7152e21e00282.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -18,75 +18,53 @@ export default function Meta() {
       />
 
       {/* HTML Meta Tags */}
-      <title key="title">Fausta | Mobile & Web Engineer</title>
+      <title key="title">{SITE_METADATA.title}</title>
       <meta
         key="description"
         name="description"
-        content="I'm a software engineer specializing in mobile and web app. Currently, I'm working as a SDE Frontend @Sayurbox"
+        content={SITE_METADATA.description}
       />
 
       {/* Google / Search Engine Tags */}
-      <meta itemProp="name" content="Fausta | Mobile & Web Engineer" />
-      <meta
-        itemProp="description"
-        content="I'm a software engineer specializing in mobile and web app. Currently, I'm working as a SDE Frontend @Sayurbox"
-      />
-      <meta
-        itemProp="image"
-        content="https://faustaleonardo.space/meta/meta.png"
-      />
+      <meta itemProp="name" content={SITE_METADATA.title} />
+      <meta itemProp="description" content={SITE_METADATA.description} />
+      <meta itemProp="image" content={SITE_METADATA.imageUrl} />
 
       {/* Facebook Meta Tags */}
-      <meta
-        key="og-url"
-        property="og:url"
-        content="https://faustaleonardo.space"
-      />
+      <meta key="og-url" property="og:url" content={SITE_METADATA.url} />
       <meta property="og:type" content="website" />
-      <meta
-        key="og-title"
-        property="og:title"
-        content="Fausta | Mobile & Web Engineer"
-      />
+      <meta key="og-title" property="og:title" content={SITE_METADATA.title} />
       <meta
         key="og-description"
         property="og:description"
-        content="I'm a software engineer specializing in mobile and web app. Currently, I'm working as a SDE Frontend @Sayurbox"
+        content={SITE_METADATA.description}
       />
       <meta
         key="og-image"
         property="og:image"
-        content="https://faustaleonardo.space/meta/meta.png"
+        content={SITE_METADATA.imageUrl}
       />
-      <meta property="og:image:width" content="776" />
-      <meta property="og:image:height" content="2122" />
-      <meta property="og:site_name" content="Fausta | Mobile & Web Engineer" />
+      <meta property="og:image:width" content={SITE_METADATA.imageHeight} />
+      <meta property="og:image:height" content={SITE_METADATA.imageWidth} />
+      <meta property="og:site_name" content={SITE_METADATA.title} />
 
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        key="tw-title"
-        name="twitter:title"
-        content="Fausta | Mobile & Web Engineer"
-      />
+      <meta key="tw-title" name="twitter:title" content={SITE_METADATA.title} />
       <meta
         key="tw-description"
         name="twitter:description"
-        content="I'm a software engineer specializing in mobile and web app. Currently, I'm working as a SDE Frontend @Sayurbox"
+        content={SITE_METADATA.description}
       />
       <meta
         key="tw-image"
         name="twitter:image"
-        content="https://faustaleonardo.space/meta/meta.png"
+        content={SITE_METADATA.imageUrl}
       />
-      <meta name="twitter:image:width" content="776" />
-      <meta name="twitter:image:height" content="2122" />
-      <meta name="twitter:creator" content="Fausta Leonardo" />
-      <meta
-        key="tw-url"
-        name="twitter:url"
-        content="https://faustaleonardo.space"
-      />
+      <meta name="twitter:image:width" content={SITE_METADATA.imageHeight} />
+      <meta name="twitter:image:height" content={SITE_METADATA.imageWidth} />
+      <meta name="twitter:creator" content={SITE_METADATA.author} />
+      <meta key="tw-url" name="twitter:url" content={SITE_METADATA.url} />
     </Head>
   );
 }
